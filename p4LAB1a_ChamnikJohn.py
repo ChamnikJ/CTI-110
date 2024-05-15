44542e2e64dc3b708a24e54a44e632a719640b47
ChamnikJ <160355748+ChamnikJ@users.noreply.github.com>
2024-05-15 10:19:02 -0400

    Add files via upload

---
+++ p4LAB1a_ChamnikJohn.py
@@ -0,0 +1,11 @@
+import turtle
+wn = turtle.Screen()
+turtle.shape("turtle")
+for i in range(4):
+    turtle.forward(50)
+    turtle.left(90)
+
+for i in range(3):
+    turtle.forward(100)          
+    turtle.left(120)            
+wn.mainloop()
